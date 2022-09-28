c38507f6c025117844a9a290b67b7a6adf3dbc71
emiyl <me@emiyl.com>
2022-09-28 23:04:04 +0100

    fix sorting

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -28,6 +28,10 @@
         fakeEntry: true
       }
       return x
+    }).sort((a,b) => {
+      if (a.name > b.name) return -1
+      if (a.name < b.name) return 1
+      return 0
     })
     
     export default {
