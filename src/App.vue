e6aec0fc9241e520d5f7b22a1da974b191e5e423
emiyl <me@emiyl.com>
2022-09-30 16:28:47 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
   
   <footer>
     <div class="footerText">
-      <p>Created by <a href="https://github.com/emiyl" target="_blank">emiyl</a>, with web design help from <a href="https://github.com/nabriouso" target="_blank">Nabrious</a>.</p>
+      <p>Created by <a href="https://github.com/emiyl" target="_blank">emiyl</a>, with website design help from <a href="https://github.com/nabriouso" target="_blank">Nabrious</a>.</p>
       <p>If you need further help, ask on the official Cemu <a href="https://discord.gg/5psYsup" target="_blank">Discord Server</a> for assistance.</p>
     </div>
   </footer>
