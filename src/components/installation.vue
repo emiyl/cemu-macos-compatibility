20f7781b59aef043c84b85cc64036153cd94ed3c
emiyl <me@emiyl.com>
2022-10-23 13:59:58 +0100

    clarified audio troubleshooting instructions

--- src/components/installation.vue
+++ src/components/installation.vue
@@ -57,7 +57,7 @@
     <h2 style="margin-bottom: 0;">Troubleshooting</h2>
     <hr>
 
-    <p><b>No sound:</b> By default, Cemu on macOS sets audio output to 0%. Go into the audio settings and turn up the volume.</p>
+    <p><b>No sound:</b> By default, Cemu on macOS sets audio output to 0%, with no audio device set. Go into the audio settings, select an audio device, and turn up the volume.</p>
     <p><b>Illegal hardware instruction</b>: Make you installed moltenvk as described above. Check graphics settings and ensure the rendering API is set to Vulkan.</p>
     <p><b>No internet</b>: Network functions are disabled on non-Windows builds as the implementation is not compatible with macOS or Linux. There is no way to bypass this currently.</p>
 
