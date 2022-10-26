53ae4f7a1323b6b01c567410afb9e98964a39e98
emiyl <me@emiyl.com>
2022-10-26 12:29:39 +0100

    added note about fast math

--- src/components/installation.vue
+++ src/components/installation.vue
@@ -19,6 +19,7 @@
     <h2 style="margin-bottom: 0;">Troubleshooting</h2>
     <hr>
 
+    <p><b>Flickering or invisible textures:</b> In some games, such as Mario Kart 8 and Bayonetta 2, some textures appear to flicker or not appear. This can be fixed by setting the environment variable <code>MVK_CONFIG_FAST_MATH_ENABLED=0</code>. This does <i>not</i> fix graphical issues in BOTW.</p>
     <p><b>No sound:</b> By default, Cemu on macOS sets audio output to 0%, with no audio device set. Go into the audio settings, select an audio device, and turn up the volume.</p>
     <p><b>Illegal hardware instruction</b>: Make you installed moltenvk as described above. Check graphics settings and ensure the rendering API is set to Vulkan.</p>
     <p><b>No internet</b>: Network functions are disabled on non-Windows builds as the implementation is not compatible with macOS or Linux. There is no way to bypass this currently.</p>
